27a045ed53e8e133c484e51d26fe041be8a72e0e
blreschke <reschke3@illinois.edu>
2018-12-04 23:30:20 -0600

    draw button & display works

--- app/src/main/java/com/example/mteag/ui_finalproject/MainActivity.java
+++ app/src/main/java/com/example/mteag/ui_finalproject/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.mteag.ui_finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import java.util.ArrayList;
+import android.widget.Button;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Deck deck = new Deck();
+    private ArrayList<Card> yourCards = new ArrayList<Card>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,33 @@ public class MainActivity extends AppCompatActivity {
         ImageView Me5 = findViewById(R.id.ME5);
         Me5.setImageResource(R.drawable.ace_of_hearts);
 
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                draw();
+            }
+        });
+    }
+
+    public void draw() {
+        Card drawnCard = deck.draw();
+        if (yourCards.size() == 0) {
+            ImageView Me1 = findViewById(R.id.ME1);
+            Me1.setImageResource(drawnCard.getImage());
+        } else if(yourCards.size() == 1) {
+            ImageView Me2 = findViewById(R.id.ME2);
+            Me2.setImageResource(drawnCard.getImage());
+        } else if(yourCards.size() == 2) {
+            ImageView Me3 = findViewById(R.id.ME3);
+            Me3.setImageResource(drawnCard.getImage());
+        } else if(yourCards.size() == 3) {
+            ImageView Me4 = findViewById(R.id.ME4);
+            Me4.setImageResource(drawnCard.getImage());
+        } else if(yourCards.size() == 4) {
+            ImageView Me5 = findViewById(R.id.ME5);
+            Me5.setImageResource(drawnCard.getImage());
+        }
+        yourCards.add(drawnCard);
     }
     //test commit
 }
